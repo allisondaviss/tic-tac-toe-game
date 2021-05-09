6f8dc498f89f6a98a39d837bd113d53c77487654
allisondaviss <77467121+allisondaviss@users.noreply.github.com>
2021-05-08 20:48:17 -0400

    Changing O and X options to A and D.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,7 +6,7 @@ def button(frame):          #Function to define a button
     return b
 def change_a():             #Function to change the operand for the next player
     global a
-    for i in ['O','X']:
+    for i in ['A','D']:
         if not(i==a):
             a=i
             break
@@ -16,7 +16,7 @@ def reset():                #Resets the game
         for j in range(3):
                 b[i][j]["text"]=" "
                 b[i][j]["state"]=NORMAL
-    a=r.choice(['O','X'])
+    a=r.choice(['A','D'])
 def check():                #Checks for victory or Draw
     for i in range(3):
             if(b[i][0]["text"]==b[i][1]["text"]==b[i][2]["text"]==a or b[0][i]["text"]==b[1][i]["text"]==b[2][i]["text"]==a):
@@ -24,7 +24,7 @@ def check():                #Checks for victory or Draw
                     reset()
     if(b[0][0]["text"]==b[1][1]["text"]==b[2][2]["text"]==a or b[0][2]["text"]==b[1][1]["text"]==b[2][0]["text"]==a):
         messagebox.showinfo("Congrats!!","'"+a+"' has won")
-        reset()   
+        reset()
     elif(b[0][0]["state"]==b[0][1]["state"]==b[0][2]["state"]==b[1][0]["state"]==b[1][1]["state"]==b[1][2]["state"]==b[2][0]["state"]==b[2][1]["state"]==b[2][2]["state"]==DISABLED):
         messagebox.showinfo("Tied!!","The match ended in a draw")
         reset()
@@ -36,8 +36,8 @@ def click(row,col):
 ###############   Main Program #################
 root=Tk()                   #Window defined
 root.title("Tic-Tac-Toe")   #Title given
-a=r.choice(['O','X'])       #Two operators defined
-colour={'O':"deep sky blue",'X':"lawn green"}
+a=r.choice(['A','D'])       #Two operators defined
+colour={'A':"deep sky blue",'D':"lawn green"}
 b=[[],[],[]]
 for i in range(3):
         for j in range(3):
